3f8ffd7c60a245e27f18d831f38078b4c3c55fc1
CMRicketts <ricketc17@gmail.com>
2019-01-31 10:56:03 -0600

    jump man works, added pausing when click off screen

--- game/main.lua
+++ game/main.lua
@@ -1,16 +1,22 @@
 function love.load()
+  fizz = require("fizzx.fizz")
 
   WINDOW_HEIGHT = love.graphics.getHeight()
   WINDOW_WIDTH = love.graphics.getWidth()
-  GRAVITY = 250
+  YGRAVITY = 250
+
+  IS_PAUSED = false
+
+  fizz.setGravity(0, YGRAVITY)
 
   love.graphics.setNewFont(12)
   love.graphics.setColor(0,0,0)
   love.graphics.setBackgroundColor(255,255,255)
 
   love.graphics.setColor(45, 45, 45)
-  character = {outline = "fill", x = 0, y = WINDOW_HEIGHT-10, width = 5, height = 10, speed = 200, yVel = 0, jumpHeight = 150}
+  character = {outline = "fill", x = 0, y = WINDOW_HEIGHT - 10, width = 5, height = 10, speed = 200, yVel = 0, jumpHeight = 150}
 
+  platform = {outline = "fill", x = 0, y = 0, height = 5, width = 30}
 end
 
 function love.draw()
@@ -20,9 +26,9 @@ end
 
 function love.focus(f)
   if not f then
-    print("LOST FOCUS")
+    IS_PAUSED = true
   else
-    print("GAINED FOCUS")
+    IS_PAUSED = false
   end
 end
 
@@ -35,35 +41,38 @@ function love.update(dt)
     love.event.quit("restart")
   end
 
-  direction = {"", ""}
-  if love.keyboard.isDown("right") then
-    direction[1] = "right"
-  elseif love.keyboard.isDown("left") then
-    direction[1] = "left"
-  else
-    direction[1] = nil
-  end
-  moveCharacter(direction, dt)
-  if love.keyboard.isDown("up") then
-    if character.yVel == 0 then
-      character.yVel = character.jumpHeight
+  if not IS_PAUSED then
+    direction = {"", ""}
+    if love.keyboard.isDown("right") then
+      direction[1] = "right"
+    elseif love.keyboard.isDown("left") then
+      direction[1] = "left"
+    else
+      direction[1] = nil
+    end
+    moveCharacter(direction, dt)
+    if love.keyboard.isDown("up") then
+      if character.yVel == 0 then
+        character.yVel = character.jumpHeight
+      end
+    end
+    if character.yVel ~= 0 then
+      character.yVel = character.yVel - YGRAVITY * dt
+      character.y = character.y - character.yVel * dt
+    end
+    if character.y >= WINDOW_HEIGHT - character.height then
+      character.yVel = 0
+      character.y = WINDOW_HEIGHT - character.height
+    end
+    if character.y <= 0 then
+      character.y = 0
+    end
+    if love.keyboard.isDown("down") then
+      character.yVel = character.yVel - YGRAVITY * dt * 2
+      character.y = character.y - character.yVel * dt * 2
     end
   end
-  if character.yVel ~= 0 then
-    print("going up by ", GRAVITY * dt)
-    print("going down by ", character.yVel * dt)
-    character.yVel = character.yVel - GRAVITY * dt
-    character.y = character.y - character.yVel * dt
-  end
-  if character.y >= WINDOW_HEIGHT - character.height then
-    character.yVel = 0
-    character.y = WINDOW_HEIGHT - character.height
-  end
-  if character.y <= 0 then
-    character.y = 0
-  end
 end
-
 
 function moveCharacter(direction, dt)
   first = direction[1]
@@ -87,38 +96,6 @@ end
 function love.quit()
   print("Thanks for playing! Come back soon!")
 end
-
---[[function creatureJump(creature, dt) 
-  ground = WINDOW_HEIGHT - character.height
-  print(ground, creature.y)
-  jumpHeight = 150
-  if creature.yVel == 0 then
-    print("vel = 0")
-    creature.yVel = jumpHeight
-  end
-  if creature.yVel ~= 0 then 
-    print("should be going up?")
-    creature.y = creature.y - creature.yVel * dt
-    creature.yVel = creature.yVel + GRAVITY * dt
-  end
-  if creature.y > WINDOW_HEIGHT - creature.height then
-    creature.yVel = 0
-    creature.y = WINDOW_HEIGHT - creature.height
-  end
-end
-
-
-function checkCollision(x, y) 
-  if x > WINDOW_WIDTH then
-    x = WINDOW_WIDTH
-  elseif x < 0 then
-    x = 0
-  elseif y > WINDOW_HEIGHT then
-    y = WINDOW_HEIGHT 
-  elseif y < 0 then
-    y = 0
-  end
-end]]
 
 
 -- "C:\Program Files\LOVE\lovec.exe" "C:\Users\ricke\Desktop\code\lua\platformForay\game"
